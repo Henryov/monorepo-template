36ef82dab31b0f0ef7c5adca8d5bed3c9d71b70e
Henry <erggcc@163.com>
2024-06-06 16:45:29 +0800

    chore: Add class-transformer and class-validator dependencies

--- service/src/modules/auth/dto/auth.dto.ts
+++ service/src/modules/auth/dto/auth.dto.ts
@@ -1,8 +1,12 @@
-
+import { IsString, Max, Min } from 'class-validator'
 export class LoginDto {
 
+    @IsString()
+    @Min(4)
     username: string
 
-
+    @IsString()
+    @Min(6)
+    @Max(16)
     password: string
 }
